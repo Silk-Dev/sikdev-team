4f5aea2543013ceb05f571207e0e4c38605a28c2
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-06-15 00:01:19 +0100

    Using docs folder to publish on GitHub pages for free ... 🙌

---
+++ docs/_astro/hoisted.572aa07f.js
@@ -0,0 +1,1 @@
+import"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js";const r=document.querySelectorAll(".userCard");r.forEach(e=>{e?.addEventListener("mouseout",()=>{e.querySelector(".roundAvatar")?.classList.add("reverseAnimation")})});
